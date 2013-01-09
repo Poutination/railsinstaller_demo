61fc42aec88ae8128d7544bed19884d1e8912c38
Josh Leslie <josh@canadianbioethics.ca>
2013-01-09 16:34:57 -0500

    First commit to repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '02e033e1042d75ba9edade524e1ef3ebabb32457dea5c2b3951e638259fb7da5f13823809fad992cb37e6c0b181f6e45893750d5d323cec8aac069949aa07089'
